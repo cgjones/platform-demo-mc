62340a6d2b51de980f1088550dfc4f2b8cdb67ae
Chris Jones <jones.chris.g@gmail.com>
2012-06-27 17:42:05 +0200

    Revert "Add dithering for opaque gradients drawn with OVER or SOURCE to 5-6-5 target"
    This reverts commit 38a3ff0cb44011a30a7d18b26c4ef91bdff7a838.

--- gfx/cairo/libpixman/src/pixman-linear-gradient.c
+++ gfx/cairo/libpixman/src/pixman-linear-gradient.c
@@ -227,58 +227,6 @@ uint16_t convert_8888_to_0565(uint32_t color)
     return CONVERT_8888_TO_0565(color);
 }
 
-#define R16_BITS     5
-#define G16_BITS     6
-#define B16_BITS     5
-
-#define R16_SHIFT    (B16_BITS + G16_BITS)
-#define G16_SHIFT    (B16_BITS)
-#define B16_SHIFT    0
-
-#define MASK 0xff
-#define ONE_HALF 0x80
-
-#define A_SHIFT 8 * 3
-#define R_SHIFT 8 * 2
-#define G_SHIFT 8
-#define A_MASK 0xff000000
-#define R_MASK 0xff0000
-#define G_MASK 0xff00
-
-#define RB_MASK 0xff00ff
-#define AG_MASK 0xff00ff00
-#define RB_ONE_HALF 0x800080
-#define RB_MASK_PLUS_ONE 0x10000100
-
-#define ALPHA_8(x) ((x) >> A_SHIFT)
-#define RED_8(x) (((x) >> R_SHIFT) & MASK)
-#define GREEN_8(x) (((x) >> G_SHIFT) & MASK)
-#define BLUE_8(x) ((x) & MASK)
-
-// This uses the same dithering technique that Skia does.
-// It is essentially preturbing the lower bit based on the
-// high bit
-static inline uint16_t dither_32_to_16(uint32_t c)
-{
-    uint8_t b = BLUE_8(c);
-    uint8_t g = GREEN_8(c);
-    uint8_t r = RED_8(c);
-    r = ((r << 1) - ((r >> (8 - R16_BITS) << (8 - R16_BITS)) | (r >> R16_BITS))) >> (8 - R16_BITS);
-    g = ((g << 1) - ((g >> (8 - G16_BITS) << (8 - G16_BITS)) | (g >> G16_BITS))) >> (8 - G16_BITS);
-    b = ((b << 1) - ((b >> (8 - B16_BITS) << (8 - B16_BITS)) | (b >> B16_BITS))) >> (8 - B16_BITS);
-    return ((r << R16_SHIFT) | (g << G16_SHIFT) | (b << B16_SHIFT));
-}
-
-uint16_t dither_8888_to_0565(uint32_t color, pixman_bool_t toggle)
-{
-    // alternate between a preturbed truncation and a regular truncation
-    if (toggle) {
-	return dither_32_to_16(color);
-    } else {
-	return CONVERT_8888_TO_0565(color);
-    }
-}
-
 static uint32_t *
 linear_get_scanline_16 (pixman_iter_t  *iter,
 			const uint32_t *mask)
@@ -288,7 +236,6 @@ linear_get_scanline_16 (pixman_iter_t  *iter,
     int             y      = iter->y;
     int             width  = iter->width;
     uint16_t *      buffer = (uint16_t*)iter->buffer;
-    int toggle             = ((x ^ y) & 1);
 
     pixman_vector_t v, unit;
     pixman_fixed_32_32_t l;
@@ -354,12 +301,9 @@ linear_get_scanline_16 (pixman_iter_t  *iter,
 	{
 	    register uint32_t color;
 
-	    color = _pixman_gradient_walker_pixel (&walker, t);
-	    while (buffer < end) {
-		// we should only dither twice instead of for every pixel
-		*buffer++ = dither_8888_to_0565(color, toggle);
-		toggle ^= 1;
-	    }
+	    color = convert_8888_to_0565(_pixman_gradient_walker_pixel (&walker, t));
+	    while (buffer < end)
+		*buffer++ = color;
 	}
 	else
 	{
@@ -370,11 +314,9 @@ linear_get_scanline_16 (pixman_iter_t  *iter,
 	    {
 		if (!mask || *mask++)
 		{
-		    *buffer = dither_8888_to_0565(_pixman_gradient_walker_pixel (&walker,
-										 t + next_inc),
-						  toggle);
+		    *buffer = convert_8888_to_0565(_pixman_gradient_walker_pixel (&walker,
+										  t + next_inc));
 		}
-		toggle ^= 1;
 		i++;
 		next_inc = inc * i;
 		buffer++;
@@ -403,10 +345,8 @@ linear_get_scanline_16 (pixman_iter_t  *iter,
 			 (dx * linear->p1.x + dy * linear->p1.y) * v2) * invden;
 		}
 
-		*buffer = dither_8888_to_0565(_pixman_gradient_walker_pixel (&walker, t),
-					      toggle);
+		*buffer = convert_8888_to_0565(_pixman_gradient_walker_pixel (&walker, t));
 	    }
-	    toggle ^= 1;
 
 	    ++buffer;
 
@@ -434,8 +374,7 @@ linear_get_scanline_wide (pixman_iter_t *iter, const uint32_t *mask)
 void
 _pixman_linear_gradient_iter_init (pixman_image_t *image, pixman_iter_t  *iter)
 {
-    // XXX: we can't use this optimization when dithering
-    if (0 && linear_gradient_is_horizontal (
+    if (linear_gradient_is_horizontal (
 	    iter->image, iter->x, iter->y, iter->width, iter->height))
     {
 	if (iter->flags & ITER_16)
